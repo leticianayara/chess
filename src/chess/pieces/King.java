5e64d80adfd6092a07c25b696f068cb84c82bc5c
Leticia Costa <leticianayaralima@gmail.com>
2025-03-07 16:34:05 -0300

    Jogada especial Roque

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,19 +2,29 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-	public King(Board borard, Color color) {
+	private ChessMatch chessMatch;
+	
+	public King(Board borard, Color color, ChessMatch chessMatch) {
 		super(borard, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece)getBorard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0; 
+	}
+
 
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece) getBorard().piece(position);
@@ -74,8 +84,36 @@ public class King extends ChessPiece {
 		if (getBorard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
+		
+		//#specialmove castling
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			//#specialmove castling kingside rook
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+			if(testRookCastling(posT1)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				if(getBorard().piece(p1) == null && getBorard().piece(p2) == null) {
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+			
+			//#specialmove castling queenside rook
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+			if(testRookCastling(posT2)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if(getBorard().piece(p1) == null && getBorard().piece(p2) == null && getBorard().piece(p3) == null) {
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
+			}
+		}
 
 		return mat;
 	}
 
+	public ChessMatch getChessMatch() {
+		return chessMatch;
+	}
+	
 }
